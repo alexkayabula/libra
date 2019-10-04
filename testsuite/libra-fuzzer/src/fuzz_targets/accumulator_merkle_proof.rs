9cf8ad88664bd2f9979f7673cd76b2b60e6b6478
David Wong <davidwg@calibra.com>
2019-10-04 16:46:02 -0700

    [fuzzing] adding merkle tree proto fuzzing
    MOTIVATION:
    
    Adding two fuzzers. When receiving sparse and non-sparse merkle tree proofs,
    there is some involved proto decoding code that we can fuzz.
    
    Closes: #1232
    Approved by: davidiw

---
+++ testsuite/libra-fuzzer/src/fuzz_targets/accumulator_merkle_proof.rs
@@ -0,0 +1,6 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use types::proof::AccumulatorProof;
+
+proto_fuzz_target!(AccumulatorProofTarget => AccumulatorProof);
---
+++ testsuite/libra-fuzzer/src/fuzz_targets/sparse_merkle_proof.rs
@@ -0,0 +1,6 @@
+// Copyright (c) The Libra Core Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+use types::proof::SparseMerkleProof;
+
+proto_fuzz_target!(SparseMerkleProofTarget => SparseMerkleProof);
